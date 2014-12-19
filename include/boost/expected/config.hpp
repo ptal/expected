42cf68d6688c6a9879c28e1eeb82a7109979eff0
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2014-12-19 12:39:06 +0100

    Try to fix issues with clang-3.6 and constexpr in c++11 mode.

--- include/boost/expected/config.hpp
+++ include/boost/expected/config.hpp
@@ -12,9 +12,11 @@
 
 #if defined BOOST_NO_CXX11_REF_QUALIFIERS
 #  define BOOST_EXPECTED_NO_CXX11_RVALUE_REFERENCE_FOR_THIS
-# endif
+#endif
 
-# if defined __clang__
+# if __cplusplus < 201400
+#  define BOOST_EXPECTED_NO_CXX11_MOVE_ACCESSORS
+# elif defined __clang__
 #  if (__clang_major__ < 3) || (__clang_major__ == 3) && (__clang_minor__ < 5)
 #   define BOOST_EXPECTED_NO_CXX11_MOVE_ACCESSORS
 #  endif
